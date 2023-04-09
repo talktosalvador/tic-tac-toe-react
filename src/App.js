d1f01ef0be3eee6846085e71f9a560744eb63abf
Salvador Vilca <talktosalvador@gmail.com>
2023-04-09 12:30:44 -0400

    adding movement history

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import {useState} from "react";
 
-function Square({value, onSquareClick}) {
-    return (
-        <button className="square" onClick={onSquareClick}>
-            {value}
-        </button>
+export default function Game () {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0)
+    const currentSquares = history[currentMove];
+
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1)
+        setXIsNext(!xIsNext);
+    }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove)
+        setXIsNext(nextMove % 2 === 0)
+    }
+
+    const moves = history.map((squares, move) => {
+        let description
+        if (move > 0) {
+            description = 'Go to move #' + move
+        } else {
+            description = 'Go to start'
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        )
+    })
+
+    return(
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+            </div>
+            <div className="game-info">
+                <ol>{moves}</ol>
+            </div>
+        </div>
     )
 }
 
@@ -47,25 +83,11 @@ function Board({ xIsNext, squares, onPlay }) {
     )
 }
 
-export default function Game () {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[history.length - 1];
-
-    function handlePlay(nextSquares) {
-        setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
-    }
-
-    return(
-        <div className="game">
-            <div className="game-board">
-                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-            </div>
-            <div className="game-info">
-                <ol>{}</ol>
-            </div>
-        </div>
+function Square({value, onSquareClick}) {
+    return (
+        <button className="square" onClick={onSquareClick}>
+            {value}
+        </button>
     )
 }
 
